14c4bda4d0ddb703d5f22a2b0c14c6228d82e2f3
Tim <tim@bathgatedesign.co.nz>
2020-10-21 17:24:42 +1300

    systemUnderTest find-replace to myPage

--- index.test.js
+++ index.test.js
@@ -1,21 +1,21 @@
-const systemUnderTest = require('./secret-sauce.js')
+const myPage = require('./secret-sauce.js')
 
-describe('the world\'s most tested heading tag', function () {
+describe('The World\'s most tested heading tag', function () {
 
     it('has an <h1>', function () {
-        const h1Element = systemUnderTest.querySelector('h1')
+        const h1Element = myPage.querySelector('h1')
 
         expect(h1Element).toBeTruthy();
     });
 
     it('has exactly one <h1>', function () {
-        const h1Element = systemUnderTest.querySelectorAll('h1')
+        const h1Element = myPage.querySelectorAll('h1')
 
         expect(h1Element.length).toEqual(1);
     });
 
     it('says "Testing the Jesting"', function () {
-        const h1Element = systemUnderTest.querySelector('h1').textContent
+        const h1Element = myPage.querySelector('h1').textContent
 
         expect(h1Element).toEqual("Testing the Jesting")
     });
